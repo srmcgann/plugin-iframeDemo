1cb2bc6660ebc544d70da0b8331c6ae3568f9bdc
Scott <smcgann@cantelope.ml>
2018-04-13 17:19:00 -0700

    modified plugin.js and rebuild webpack

--- plugin.js
+++ plugin.js
@@ -2,23 +2,20 @@ kiwi.plugin('iframeDemoPlugin', function(kiwi, log) {
 
   let framesVisible = buttonAdded = false;
   
-  //kiwi.on('irc.raw', function(command, event){
-    if(!buttonAdded){
-      //alert(kiwi.state.setting('iframeDemo.url'));
-      buttonAdded = true;
-      const iframesDemoTool = document.createElement('i');
-      iframesDemoTool.className = 'fa fa-address-card';
-      kiwi.addUi('input', iframesDemoTool);
-      iframesDemoTool.onclick = function(e){ 
-        e.preventDefault();
-        if(framesVisible){
-          hideFrame();
-        }else{
-          showFrame();
-        }
+  if(!buttonAdded){
+    buttonAdded = true;
+    const iframesDemoTool = document.createElement('i');
+    iframesDemoTool.className = 'fa fa-address-card';
+    kiwi.addUi('input', iframesDemoTool);
+    iframesDemoTool.onclick = function(e){ 
+      e.preventDefault();
+      if(framesVisible){
+        hideFrame();
+      }else{
+        showFrame();
       }
     }
-  //});
+  }
   
   function iframeize(){
     kiwi.emit('mediaviewer.show', kiwi.state.setting('iframeDemo.url'), true);
